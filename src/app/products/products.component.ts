3979bd2902655b560acce242002444ff728880fe
radhouene-massoudi <massoudiradhouene@gmail.com>
2023-09-19 15:47:58 +0100

    seance 1+2

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from '../model/product';
 
 @Component({
   selector: 'app-products',
@@ -7,7 +8,7 @@ import { Component } from '@angular/core';
 })
 export class ProductsComponent {
   nameOfProduct='';
-  listProducts=[
+  listProducts:Product[]=[
     {id: 1, title: "T-shirt 1", price: 18, quantity: 0, like: 0},
     {id: 2, title: "T-shirt 2", price: 21, quantity: 10, like: 0},
     {id: 3, title: "T-shirt 3", price: 16, quantity: 8, like: 0}, ]
